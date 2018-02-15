b102f8ff87a5178cedd0fdbc468fa942eba2d69a
Andrew Park <apark18@users.noreply.github.com>
2018-02-15 09:23:03 -0500

    Update Sort.java

--- src/com/apark18/sorting/Sort.java
+++ src/com/apark18/sorting/Sort.java
@@ -1,6 +1,9 @@
 package com.apark18.sorting;
 
+import java.util.*;
+
 public class Sort {
+	
 	public static void bubbleSort(int[] array) {
 		int temp;
 		for (int i = 0; i < array.length - 1; i++) {
@@ -13,46 +16,47 @@ public class Sort {
 			}
 		}
 	}
-  
-  public static void combSort(int[] array) {
+
+	public static void combSort(int[] array) {
 		int gap = array.length;
-		boolean swapped = true; 
+		boolean swapped = true;
 		int i;
 		while (gap > 1 || swapped) {
-			if (gap > 1) 
-				gap = (int)(gap / 1.3);
-			if (gap == 9 || gap == 10) 
+			if (gap > 1)
+				gap = (int) (gap / 1.3);
+			if (gap == 9 || gap == 10)
 				gap = 11;
 			i = 0;
 			swapped = false;
 			while (i + gap < array.length) {
-				if (array[i] > array[i+gap]) {
-					swapC(array, i, i+gap);
+				if (array[i] > array[i + gap]) {
+					swapC(array, i, i + gap);
 					swapped = true;
 				}
 				i++;
 			}
-		} 
+		}
 	}
-	public static void swapC(int[] list, int i, int j) {
+
+	private static void swapC(int[] list, int i, int j) {
 		int temp = list[i];
 		list[i] = list[j];
 		list[j] = temp;
 	}
-  
-  public static void insertionSort(int[] array){
-		for(int i = 1; i < array.length; i++){
+
+	public static void insertionSort(int[] array) {
+		for (int i = 1; i < array.length; i++) {
 			int temp = array[i];
 			int possibleIndex = i;
-			while(possibleIndex > 0 && temp < array[possibleIndex - 1]){
+			while (possibleIndex > 0 && temp < array[possibleIndex - 1]) {
 				array[possibleIndex] = array[possibleIndex - 1];
 				possibleIndex--;
 			}
 			array[possibleIndex] = temp;
 		}
 	}
-  
-  public static void mergeSort(int [] array) {
+
+	public static void mergeSort(int[] array) {
 		if (array.length <= 1) {
 			return;
 		}
@@ -64,8 +68,8 @@ public class Sort {
 		mergeSort(second);
 		merge(first, second, array);
 	}
-	    
-	private static void merge(int[] first, int[] second, int [] result) {
+
+	private static void merge(int[] first, int[] second, int[] result) {
 		int iFirst = 0;
 		int iSecond = 0;
 		int j = 0;
@@ -76,58 +80,64 @@ public class Sort {
 			} else {
 				result[j] = second[iSecond];
 				iSecond++;
-	            	}
-	            j++;	
-	        }	
+			}
+			j++;
+		}
 		System.arraycopy(first, iFirst, result, j, first.length - iFirst);
 		System.arraycopy(second, iSecond, result, j, second.length - iSecond);
 	}
-  
-  private static int arr[];
-	public static void quickSort(int[] array){
+
+	private static int arr[];
+
+	public static void quickSort(int[] array) {
 		arr = array;
 		int left = 0;
-		int right = arr.length-1;
-			
+		int right = arr.length - 1;
+
 		quickSort(left, right);
 	}
-	private static void quickSort(int left, int right){
-		
-		if(left >= right)
+
+	private static void quickSort(int left, int right) {
+
+		if (left >= right)
 			return;
-		
+
 		int pivot = arr[right];
 		int partition = partition(left, right, pivot);
-		
-		quickSort(0, partition-1);
-		quickSort(partition+1, right);
+
+		quickSort(0, partition - 1);
+		quickSort(partition + 1, right);
 	}
-	private static int partition(int left,int right,int pivot){
-		int leftCursor = left-1;
+
+	private static int partition(int left, int right, int pivot) {
+		int leftCursor = left - 1;
 		int rightCursor = right;
-		while(leftCursor < rightCursor){
-                while(arr[++leftCursor] < pivot);
-                while(rightCursor > 0 && arr[--rightCursor] > pivot);
-			if(leftCursor >= rightCursor){
+		while (leftCursor < rightCursor) {
+			while (arr[++leftCursor] < pivot)
+				;
+			while (rightCursor > 0 && arr[--rightCursor] > pivot)
+				;
+			if (leftCursor >= rightCursor) {
 				break;
-			}else{
+			} else {
 				swapQ(leftCursor, rightCursor);
 			}
 		}
 		swapQ(leftCursor, right);
 		return leftCursor;
 	}
-	public static void swapQ(int left,int right){
+
+	private static void swapQ(int left, int right) {
 		int temp = arr[left];
 		arr[left] = arr[right];
 		arr[right] = temp;
 	}
-  
-  public static void selectionSort(int[] array){
-		for(int i = 0; i < array.length - 1; i++){
+
+	public static void selectionSort(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
 			int minIndex = i;
-			for(int j = i + 1; j < array.length; j++){
-				if(array[j] < array[minIndex]){
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[minIndex]) {
 					minIndex = j;
 				}
 			}
@@ -135,8 +145,9 @@ public class Sort {
 			array[i] = array[minIndex];
 			array[minIndex] = temp;
 		}
-  }
-    public static void shellSort(int[] array) {
+	}
+
+	public static void shellSort(int[] array) {
 		int inner, outer;
 		int temp;
 		int h = 1;
@@ -155,6 +166,34 @@ public class Sort {
 			}
 			h = (h - 1) / 3;
 		}
-		
 	}
+
+	public static void radixLSDSort(int[] arr) {
+		Queue<Integer>[] buckets = new Queue[10];
+		for (int i = 0; i < 10; i++)
+			buckets[i] = new LinkedList<Integer>();
+
+		boolean sorted = false;
+		int expo = 1;
+
+		while (!sorted) {
+			sorted = true;
+
+			for (int item : arr) {
+				int bucket = (item / expo) % 10;
+				if (bucket > 0)
+					sorted = false;
+				buckets[bucket].add(item);
+			}
+
+			expo *= 10;
+			int index = 0;
+
+			for (Queue<Integer> bucket : buckets)
+				while (!bucket.isEmpty())
+					arr[index++] = bucket.remove();
+		}
+
+	}
+
 }
